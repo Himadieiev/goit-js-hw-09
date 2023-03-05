21ef2327dc0e19d08d6e42af4c56717d966f1c2a
Ruslan Himadieiev <gimadejev86@gmail.com>
2023-03-05 19:56:12 +0200

    add

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -6,18 +6,14 @@ const btnStart = document.querySelector('button[data-start]');
 const btnStop = document.querySelector('button[data-stop]');
 const body = document.querySelector('body');
 const INTERVAL = 1000;
-let isActive = false;
 let timerID = null;
+btnStart.disabled = false;
 
 btnStart.addEventListener('click', onStart);
 btnStop.addEventListener('click', onStop);
 
-function onStart() {
-  body.style.backgroundColor = getRandomHexColor();
-  if (isActive) {
-    return;
-  }
-  isActive = true;
+function onStart() {  
+  btnStart.disabled = true;
   timerID = setInterval(() => {
     body.style.backgroundColor = getRandomHexColor();
   }, INTERVAL);
@@ -25,5 +21,5 @@ function onStart() {
 
 function onStop() {
   clearInterval(timerID);
-  isActive = false;
+  btnStart.disabled = false;
 }
